bb19d35643591a320d7a02cf5b6d3acb1f5029c3
Mohamed Emad <mohamedemadfahmy0@gmail.com>
2024-11-16 15:43:55 +0200

    Make the player at 0 0 0 and draw gridlines and make camera moveable

--- OpenGL3DTemplate.cpp
+++ OpenGL3DTemplate.cpp
@@ -1,83 +1,349 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <glut.h>
 
 int screenWidth = 1000;
 int screenHeight = 1000;
 
+#define GLUT_KEY_ESCAPE 27
+#define DEG2RAD(a) (a * 0.0174532925)
+
+class Vector3f {
+public:
+    float x, y, z;
+
+    Vector3f(float _x = 0.0f, float _y = 0.0f, float _z = 0.0f) {
+        x = _x;
+        y = _y;
+        z = _z;
+    }
+
+    Vector3f operator+(Vector3f& v) {
+        return Vector3f(x + v.x, y + v.y, z + v.z);
+    }
+
+    Vector3f operator-(Vector3f& v) {
+        return Vector3f(x - v.x, y - v.y, z - v.z);
+    }
+
+    Vector3f operator*(float n) {
+        return Vector3f(x * n, y * n, z * n);
+    }
+
+    Vector3f operator/(float n) {
+        return Vector3f(x / n, y / n, z / n);
+    }
+
+    Vector3f unit() {
+        return *this / sqrt(x * x + y * y + z * z);
+    }
+
+    Vector3f cross(Vector3f v) {
+        return Vector3f(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x);
+    }
+};
+
+class Camera {
+public:
+    Vector3f eye, center, up;
+
+    Camera(float eyeX = 3.0f, float eyeY = 3.0f, float eyeZ = 3.0f, float centerX = 0.0f, float centerY = 0.0f, float centerZ = 0.0f, float upX = 0.0f, float upY = 1.0f, float upZ = 0.0f) {
+        eye = Vector3f(eyeX, eyeY, eyeZ);
+        center = Vector3f(centerX, centerY, centerZ);
+        up = Vector3f(upX, upY, upZ);
+    }
+
+    void moveX(float d) {
+        Vector3f right = up.cross(center - eye).unit();
+        eye = eye + right * d;
+        center = center + right * d;
+    }
+
+    void moveY(float d) {
+        eye = eye + up.unit() * d;
+        center = center + up.unit() * d;
+    }
+
+    void moveZ(float d) {
+        Vector3f view = (center - eye).unit();
+        eye = eye + view * d;
+        center = center + view * d;
+    }
+
+    void rotateX(float a) {
+        Vector3f view = (center - eye).unit();
+        Vector3f right = up.cross(view).unit();
+        view = view * cos(DEG2RAD(a)) + up * sin(DEG2RAD(a));
+        up = view.cross(right);
+        center = eye + view;
+    }
+
+    void rotateY(float a) {
+        Vector3f view = (center - eye).unit();
+        Vector3f right = up.cross(view).unit();
+        view = view * cos(DEG2RAD(a)) + right * sin(DEG2RAD(a));
+        right = view.cross(up);
+        center = eye + view;
+    }
+
+    void look() {
+        gluLookAt(
+            eye.x, eye.y, eye.z,
+            center.x, center.y, center.z,
+            up.x, up.y, up.z
+        );
+    }
+};
+
+Camera camera;
+
 void drawWall(double thickness) {
-	glPushMatrix();
-	glTranslated(0.5, 0.5 * thickness, 0.5);
-	glScaled(1.0, thickness, 1.0);
-	glutSolidCube(1);
-	glPopMatrix();
+    glPushMatrix();
+
+    // Enable blending for transparency
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); // Set blending function
+
+    // Set the color to green with some transparency (0.5 is 50% transparent)
+    glColor4f(0.0f, 1.0f, 0.0f, 0.5f);  // RGBA, where A is the alpha value
+
+    // Transform the wall
+    glTranslated(0.5, 0.5 * thickness, 0.5);
+    glScaled(10.0, thickness, 10.0);
+
+    // Draw the wall (as a solid cube)
+    glutSolidCube(1);
+
+    // Disable blending after drawing the transparent object
+    glDisable(GL_BLEND);
+
+    glPopMatrix();
+}
+
+
+void drawGridlines() {
+    glPushMatrix();
+
+    glLineWidth(10.0f);  // Adjust the line width here
+
+    // Draw X-axis (Red)
+    glColor3f(1.0f, 0.0f, 0.0f); // Red color
+    glBegin(GL_LINES);
+    glVertex3f(0.0f, 0.0f, 0.0f);  // Start at the origin
+    glVertex3f(10.0f, 0.0f, 0.0f);  // End point along the X-axis
+    glEnd();
+
+    // Draw Y-axis (Yellow)
+    glColor3f(1.0f, 1.0f, 0.0f); // Yellow color
+    glBegin(GL_LINES);
+    glVertex3f(0.0f, 0.0f, 0.0f);  // Start at the origin
+    glVertex3f(0.0f, 10.0f, 0.0f);  // End point along the Y-axis
+    glEnd();
+
+    // Draw Z-axis (Blue)
+    glColor3f(0.0f, 0.0f, 1.0f); // Blue color
+    glBegin(GL_LINES);
+    glVertex3f(0.0f, 0.0f, 0.0f);  // Start at the origin
+    glVertex3f(0.0f, 0.0f, 10.0f);  // End point along the Z-axis
+    glEnd();
+
+    glPopMatrix();
+}
+
+
+
+void drawWalls() {
+    drawWall(0.01);
+
+   /* glPushMatrix();
+    glRotated(90, 0, 0, 1.0);
+    drawWall(0.02);
+    glPopMatrix();
+
+    glPushMatrix();
+    glRotated(-90, 1.0, 0.0, 0.0);
+    drawWall(0.02);
+    glPopMatrix();*/
 }
 
 void setupLights() {
-	GLfloat ambient[] = { 0.7f, 0.7f, 0.7, 1.0f };
-	GLfloat diffuse[] = { 0.6f, 0.6f, 0.6, 1.0f };
-	GLfloat specular[] = { 1.0f, 1.0f, 1.0, 1.0f };
-	GLfloat shininess[] = { 50 };
-	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, specular);
-	glMaterialfv(GL_FRONT, GL_SHININESS, shininess);
+    GLfloat ambient[] = { 0.7f, 0.7f, 0.7f, 1.0f };
+    GLfloat diffuse[] = { 0.6f, 0.6f, 0.6f, 1.0f };
+    GLfloat specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+    GLfloat shininess[] = { 50 };
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, shininess);
 
-	GLfloat lightIntensity[] = { 0.7f, 0.7f, 1, 1.0f };
-	GLfloat lightPosition[] = { -7.0f, 6.0f, 3.0f, 0.0f };
-	glLightfv(GL_LIGHT0, GL_POSITION, lightIntensity);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightIntensity);
+    GLfloat lightIntensity[] = { 0.7f, 0.7f, 1.0f, 1.0f };
+    GLfloat lightPosition[] = { -7.0f, 6.0f, 3.0f, 0.0f };
+    glLightfv(GL_LIGHT0, GL_POSITION, lightIntensity);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightIntensity);
 }
 
 void setupCamera() {
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(-0.5, 0.5, -0.5, 0.5, -1, 1);
-	//gluPerspective(60, 640 / 480, 0.001, 100);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60, 1000/1000, 0.001, 100);
 
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(0.1, 0.1, 0.1, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    camera.look();
+}
+
+void drawPlayer(float x, float y, float z) {
+
+    // Head (Sphere)
+    glPushMatrix();
+    glTranslated(x, y + 0.9, z); // Position for the head relative to the center
+    glColor3f(1.0, 0.8, 0.6);
+    glutSolidSphere(0.1, 20, 20);
+    glPopMatrix();
+
+    // Scarf (Cylinder)
+    glPushMatrix();
+    glTranslated(x, y + 0.9, z); // Position around the neck relative to the center
+    glRotated(90, 1, 0, 0); // Rotate to align with the neck
+    glColor3f(1.0, 0.0, 0.0);
+    GLUquadric* quad = gluNewQuadric();
+    gluCylinder(quad, 0.11, 0.11, 0.1, 20, 20); // Cylinder for the scarf
+    glPopMatrix();
+
+    // Body (Cuboid)
+    glPushMatrix();
+    glTranslated(x, y + 0.6, z); // Center position for the body
+    //glColor3f(0.0, 0.6, 1);
+    glColor3f(0.2, 0.2, 0.25);
+    glScaled(0.2, 0.4, 0.1); // Scale to form a cuboid shape
+    glutSolidCube(1.0);
+    glPopMatrix();
+
+    // Left Arm (Cylinder)
+    glPushMatrix();
+    glTranslated(x - 0.11, y + 0.8, z); // Position for the left arm relative to the center
+    //glRotated(-30, 0, 0, 1); // Rotate slightly to show a natural bend
+    //glColor3f(0.2, 0.5, 1);
+    glRotated(70, 1, 0, 0); // Rotate slightly to show a natural bend
+    gluCylinder(quad, 0.02, 0.02, 0.3, 20, 20); // Cylinder for the left arm
+    glPopMatrix();
+
+    // Right Arm (Cylinder)
+    glPushMatrix();
+    glTranslated(x + 0.11, y + 0.8, z); // Position for the right arm relative to the center
+    //glRotated(-30, 0, 0, 1); // Rotate slightly to show a natural bend
+    glRotated(70, 1, 0, 0); // Rotate slightly to show a natural bend
+    gluCylinder(quad, 0.02, 0.02, 0.3, 20, 20); // Cylinder for the right arm
+    glPopMatrix();
+
+    // Left Leg (Cylinder)
+    glPushMatrix();
+    glTranslated(x - 0.05, y + 0.4, z); // Position for the left leg relative to the center
+    glRotated(110, 1, 0, 0); // Slight rotation for natural stance
+    gluCylinder(quad, 0.03, 0.03, 0.4, 20, 20); // Cylinder for the left leg
+    glPopMatrix();
+
+    // Right Leg (Cylinder)
+    glPushMatrix();
+    glTranslated(x + 0.05, y + 0.4, z); // Position for the right leg relative to the center
+    glRotated(80, 1, 0, 0); // Slight rotation for natural stance
+    gluCylinder(quad, 0.03, 0.03, 0.4, 20, 20); // Cylinder for the right leg
+    glPopMatrix();
+
+    gluDeleteQuadric(quad);
 }
 
 void Display() {
-	setupLights();
-	setupCamera();
+    setupCamera();
+    setupLights();
 
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    drawWalls();
+	drawPlayer(0.1, 0.1, 0.1);
+	drawGridlines();
 
-	drawWall(0.02);
+    //// Adjust the camera to follow the skier if not in free-cam mode
+    //if (view != "free") {
+    //    camera.eye = Vector3f(skierPosition.x, skierPosition.y + 0.5f, skierPosition.z + 1.5f);
+    //    camera.center = skierPosition;
+    //    camera.look();
+    //}
 
-	glPushMatrix();
-	glRotated(90, 0, 0, 1.0);
-	drawWall(0.02);
-	glPopMatrix();
+    glFlush();
+}
 
-	glPushMatrix();
-	glRotated(-90, 1.0, 0.0, 0.0);
-	drawWall(0.02);
-	glPopMatrix();
+void Keyboard(unsigned char key, int x, int y) {
+    float d = 0.05;
 
-	glFlush();
+    switch (key) {
+    case 'w':
+        camera.moveY(d);
+        break;
+    case 's':
+        camera.moveY(-d);
+        break;
+    case 'a':
+        camera.moveX(d);
+        break;
+    case 'd':
+        camera.moveX(-d);
+        break;
+    case 'q':
+        camera.moveZ(d);
+        break;
+    case 'e':
+        camera.moveZ(-d);
+        break;
+
+    case GLUT_KEY_ESCAPE:
+        exit(EXIT_SUCCESS);
+    }
+
+    glutPostRedisplay();
+}
+
+void Special(int key, int x, int y) {
+    float a = 1.0;
+
+    switch (key) {
+    case GLUT_KEY_UP:
+        camera.rotateX(a);
+        break;
+    case GLUT_KEY_DOWN:
+        camera.rotateX(-a);
+        break;
+    case GLUT_KEY_LEFT:
+        camera.rotateY(a);
+        break;
+    case GLUT_KEY_RIGHT:
+        camera.rotateY(-a);
+        break;
+    }
+
+    glutPostRedisplay();
 }
 
 void main(int argc, char** argv) {
-	glutInit(&argc, argv);
+    glutInit(&argc, argv);
 
-	glutInitWindowSize(screenWidth, screenHeight);
-	glutInitWindowPosition(0, 0);
+    glutInitWindowSize(screenWidth, screenHeight);
+    glutCreateWindow("Golfito");
+    glutDisplayFunc(Display);
+    glutKeyboardFunc(Keyboard);
+    glutSpecialFunc(Special);
 
-	glutCreateWindow("Gameito");
-	glutDisplayFunc(Display);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
 
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_NORMALIZE);
+    glEnable(GL_COLOR_MATERIAL);
 
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-	glEnable(GL_NORMALIZE);
-	glEnable(GL_COLOR_MATERIAL);
+    glShadeModel(GL_SMOOTH);
 
-	glShadeModel(GL_SMOOTH);
-
-	glutMainLoop();
+    glutMainLoop();
 }
